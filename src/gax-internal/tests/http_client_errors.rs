b29861cd62e8391f0c7de31f7bc66e227c7e3b53
Wes Tarle <westarle@google.com>
2025-10-06 15:49:40 -0400

    test(gax-internal): Add tracing tests for http_client_errors

--- src/gax-internal/tests/http_client_errors.rs
+++ src/gax-internal/tests/http_client_errors.rs
@@ -15,7 +15,10 @@
 #[cfg(all(test, feature = "_internal-http-client"))]
 mod tests {
     use gax::options::*;
+    use google_cloud_test_utils::test_layer::TestLayer;
+    use opentelemetry_semantic_conventions::trace as semconv;
     use serde_json::json;
+    use test_case::test_case;
 
     type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -23,16 +26,26 @@ mod tests {
         auth::credentials::anonymous::Builder::new().build()
     }
 
+    #[test_case(false; "tracing off")]
+    #[test_case(true; "tracing on")]
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-    async fn test_error_with_status() -> Result<()> {
+    async fn test_error_with_status(enable_tracing: bool) -> Result<()> {
+        if enable_tracing && !cfg!(feature = "_unstable-o12y") {
+            // Skip this test if the feature is disabled.
+            return Ok(());
+        }
+
         use serde_json::Value;
         let (endpoint, _server) = echo_server::start().await?;
+        let guard = TestLayer::initialize();
 
-        let client = echo_server::builder(endpoint)
-            .with_credentials(test_credentials())
-            .build()
-            .await?;
-
+        let client_builder = echo_server::builder(endpoint).with_credentials(test_credentials());
+        let client_builder = if enable_tracing {
+            client_builder.with_tracing()
+        } else {
+            client_builder
+        };
+        let client = client_builder.build().await?;
         let builder = client.builder(reqwest::Method::GET, "/error".into());
         let body = json!({});
         let response = client
@@ -48,6 +61,21 @@ mod tests {
                 let got = e.status();
                 let want = echo_server::make_status()?;
                 assert_eq!(got, Some(&want));
+
+                let spans = TestLayer::capture(&guard);
+                if enable_tracing {
+                    assert_eq!(spans.len(), 1, "Expected 1 span");
+                    let span = &spans[0];
+                    assert_eq!(span.name, "http_request");
+                    let expected_status_code = http::StatusCode::BAD_REQUEST.as_u16();
+                    assert_eq!(
+                        span.attributes.get(semconv::ERROR_TYPE),
+                        Some(&expected_status_code.to_string()),
+                        "error.type should be the HTTP status code"
+                    );
+                } else {
+                    assert_eq!(spans.len(), 0, "Expected 0 spans when tracing is disabled");
+                }
             }
         }
 
